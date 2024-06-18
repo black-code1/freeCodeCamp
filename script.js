fc8173c9cf3d9460128248d7f386a4657e2af004
black-code <legrandmahamat1@gmail.com>
2024-06-18 10:13:15 +0100

    Learn Introductory JavaScript by Building a Pyramid Generator - step 46

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const character = "#";
 const count = 8;
 const rows = [];
+
+function padRow() {}
+
 for (let i = 0; i < count; i = i + 1) {
     rows.push(character.repeat(i + i));
 }
+
 let result = "";
+
 for (const row of rows) {
     result = result + "\n" + row;
 }
