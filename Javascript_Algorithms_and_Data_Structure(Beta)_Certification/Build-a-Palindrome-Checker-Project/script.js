6b0e2360ea4b7c4deffe0af953d8ab1add35f7d6
black-code <legrandmahamat1@gmail.com>
2024-07-06 14:53:17 +0100

    Build a Palindrome Checker Project - 5.When the #text-input element only contains the letter A and the #check-btn element is clicked, the #result element should contain the text "A is a palindrome"

--- Javascript_Algorithms_and_Data_Structure(Beta)_Certification/Build-a-Palindrome-Checker-Project/script.js
+++ Javascript_Algorithms_and_Data_Structure(Beta)_Certification/Build-a-Palindrome-Checker-Project/script.js
@@ -1,7 +1,10 @@
 const checkBtn = document.querySelector('#check-btn');
 const textInput = document.querySelector('#text-input');
+const result = document.querySelector('#result');
 checkBtn.addEventListener('click', () => {
     if (textInput.value === '') {
         alert("Please input a value")
+    } else if (textInput.value.length === 1) {
+        result.innerText = `${textInput.value} is a palindrome`;
     }
 })
