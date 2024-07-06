bfaf4e640a1f94ec304ee0eefd6a7405a7d84462
black-code <legrandmahamat1@gmail.com>
2024-07-06 16:25:36 +0100

    Build a Palindrome Checker Project - 7.When the #text-input element contains the text _eye and the #check-btn element is clicked, the #result element should contain the text "_eye is a palindrome"

--- Javascript_Algorithms_and_Data_Structure(Beta)_Certification/Build-a-Palindrome-Checker-Project/script.js
+++ Javascript_Algorithms_and_Data_Structure(Beta)_Certification/Build-a-Palindrome-Checker-Project/script.js
@@ -2,11 +2,12 @@ const checkBtn = document.querySelector('#check-btn');
 const textInput = document.querySelector('#text-input');
 const result = document.querySelector('#result');
 checkBtn.addEventListener('click', () => {
+    const textInputRegex = textInput.value.replace(/[^A-Za-z0-9]/g, '')
     if (textInput.value === '') {
         alert("Please input a value")
     } else if (textInput.value.length === 1) {
         result.innerText = `${textInput.value} is a palindrome`;
-    } else if(textInput.value === [...textInput.value].reverse().join("")) {
+    } else if(textInputRegex === [...textInputRegex].reverse().join("")) {
         result.innerText = `${textInput.value} is a palindrome`
     }
 })
