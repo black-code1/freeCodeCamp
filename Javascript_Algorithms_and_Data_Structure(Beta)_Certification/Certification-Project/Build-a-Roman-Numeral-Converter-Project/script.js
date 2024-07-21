d8a81c6e811e8650d0bab08e6a2556b83cbea35e
Mahamat Legrand <legrandmahamat1@gmail.com>
2024-07-21 11:18:13 +0100

    Build a Roman Numeral Converter

--- Javascript_Algorithms_and_Data_Structure(Beta)_Certification/Certification-Project/Build-a-Roman-Numeral-Converter-Project/script.js
+++ Javascript_Algorithms_and_Data_Structure(Beta)_Certification/Certification-Project/Build-a-Roman-Numeral-Converter-Project/script.js
@@ -1,7 +1,78 @@
 const convertBtn = document.getElementById('convert-btn');
 const numberInput = document.getElementById('number');
-const output = document.getElementById('output');
+let output = document.getElementById('output');
 
+const romanNumeralData = [
+    {
+        romanNumeral: 'M',
+        arabicNumeral: 1000,
+    },
+    {
+        romanNumeral: 'CM',
+        arabicNumeral: 900,
+    },
+    {
+        romanNumeral: 'D',
+        arabicNumeral: 500,
+    },
+    {
+        romanNumeral: 'CD',
+        arabicNumeral: 400,
+    }
+    , {
+        romanNumeral: 'C',
+        arabicNumeral: 100,
+    },
+    {
+        romanNumeral: 'XC',
+        arabicNumeral: 90,
+    },
+    {
+        romanNumeral: 'L',
+        arabicNumeral: 50,
+    },
+    {
+        romanNumeral: 'XL',
+        arabicNumeral: 40,
+    },
+    {
+        romanNumeral: 'X',
+        arabicNumeral: 10,
+    },
+    {
+        romanNumeral: 'IX',
+        arabicNumeral: 9,
+    }
+    , {
+        romanNumeral: 'V',
+        arabicNumeral: 5,
+    },
+    {
+        romanNumeral: 'IV',
+        arabicNumeral: 4,
+    },
+    {
+        romanNumeral: 'I',
+        arabicNumeral: 1,
+    },
+]
+
+const decimalToRomanNumeral = (input, result = '') => {
+    if ( input < 0) {
+        return "Please enter a number greater than or equal to 1"
+    } else if (input >= 4000) {
+        return "Please enter a number less than or equal to 3999"
+    } else {
+        for (let i = 0; i < romanNumeralData.length; i++) {
+            const { romanNumeral, arabicNumeral } = romanNumeralData[i];
+            if (input >= arabicNumeral) {
+                return decimalToRomanNumeral(input - arabicNumeral, result + romanNumeral);
+            }
+        }
+        return result;
+    }
+
+}
 const checkUserInput = () => {
     const inputInt = parseInt(numberInput.value)
 
@@ -11,27 +82,9 @@ const checkUserInput = () => {
     ){
         output.innerText = "Please enter a valid number"
         return
-    } else if( inputInt < 0) {
-        output.innerText = "Please enter a number greater than or equal to 1"
-        return
-    } else if (inputInt >= 4000) {
-        output.innerText = "Please enter a number less than or equal to 3999"
-        return
-    } else if (inputInt === 9) {
-        output.innerText = "IX"
-        return
-    } else if (inputInt === 16) {
-        output.innerText = "XVI"
-        return
-    } else if (inputInt === 649) {
-        output.innerText = "DCXLIX"
-        return
-    } else if (inputInt === 1023) {
-        output.innerText = "MXXIII"
-        return
-    } else if (inputInt === 3999) {
-        output.innerText = "MMMCMXCIX"
-        return
+    }  else {
+        output.textContent = decimalToRomanNumeral(inputInt)
+        numberInput.value = ''
     }
 }
 
